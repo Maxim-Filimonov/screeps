d11d1e993f005675a580d5f17303c09aaef2e928
Maxim Filimonov <tpaktopsp@gmail.com>
2017-06-21 16:55:44 +0800

    remove typings from under source control

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/screeps/index.d.ts" />
+/// <reference path="modules/webpack/index.d.ts" />
